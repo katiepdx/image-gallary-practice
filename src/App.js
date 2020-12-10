835f1364781045fa410dfe096b05d5fd28205b29
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-09 17:14:01 -0800

    add styles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1>Image Gallery Practice</h1>
-
-      <ImageSlider />
+      <section>
+        <div>
+          <h1>Project 1</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum. Nulla facilisi cras fermentum odio. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Proin libero nunc consequat interdum varius sit. Malesuada pellentesque elit eget gravida cum. Diam ut venenatis tellus in. Id consectetur purus ut faucibus pulvinar. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Nascetur ridiculus mus mauris vitae ultricies leo. Vitae tortor condimentum lacinia quis vel. Ac tortor dignissim convallis aenean et tortor at risus viverra.</p>
+        </div>
+        <ImageSlider />
+      </section>
     </div>
   );
 }
